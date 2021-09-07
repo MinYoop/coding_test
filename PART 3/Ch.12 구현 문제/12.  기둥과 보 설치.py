db2395348fa86cd744f368bd897831745df3072e
MinYoop <myk430s@naver.com>
2021-09-07 19:38:58 +0900

    09/07 구현 12. 기둥과 보 설치

---
+++ PART 3/Ch.12 구현 문제/12.  기둥과 보 설치.py
@@ -0,0 +1,33 @@
+def possible(answer):
+    
+    for x, y, a in answer:
+
+        if a == 0: # 기둥 : 바닥 위 / 보 양쪽 끝 / 기둥 위
+            if y == 0 or [x-1,y,1] in answer or [x,y,1] in answer or [x,y-1,0] in answer:
+                continue
+            return False
+
+        else: # 보 : 기둥 위 / 양쪽 끝이 보랑 연결
+
+            if [x,y-1,0] in answer or [x+1,y-1,0] in answer or ([x-1,y,1] in answer and [x+1,y,1] in answer):
+                continue
+            return False
+    
+    return True
+
+
+def solution(n, build_frame):
+    answer = []
+
+    for frame in build_frame:
+        x, y, a, b = frame
+
+        if b == 1:
+            answer.append([x,y,a])
+            if not possible(answer):
+                answer.remove([x,y,a])
+        else:
+            answer.remove([x,y,a])
+            if not possible(answer):
+                answer.append([x,y,a])
+    return sorted(answer) 
