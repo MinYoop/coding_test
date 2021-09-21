53ef5c960e9d5d6d76e0f8f152d6315c547fcd02
MinYoop <myk430s@naver.com>
2021-09-21 21:03:30 +0900

    09/21 이진 탐색 문제 28. 고정점 찾기

---
+++ PART 3/Ch.15 이진 탐색 문제/28. 고정점 찾기.py
@@ -0,0 +1,19 @@
+
+n = int(input())
+lst = list(map(int,input().split()))
+
+
+def binary(start, end):
+    
+    if start > end:
+        return -1
+    mid = (start + end) // 2
+
+    if mid == lst[mid]:
+        return mid
+    elif mid < lst[mid]:
+        return binary(start, mid-1)
+    else:
+        return binary(mid+1, end)
+
+print(binary(0,n-1))
