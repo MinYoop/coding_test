d3cee32bdb7457cbd886d635b81818388e3ef904
MinYoop <myk430s@naver.com>
2021-10-07 21:15:11 +0900

    10/07 DP 문제 36. 편집 거리

---
+++ PART 3/Ch.16 다이나믹 프로그래밍 문제/36. 편집 거리.py
@@ -0,0 +1,28 @@
+
+a = input()
+b = input()
+
+dp = [[0] * (len(b)+1) for _ in range(len(a)+1)]
+
+for i in range(len(b)+1):
+
+    dp[0][i] = i
+
+for i in range(len(a)+1):
+
+    dp[i][0] = i
+
+for i in range(1,len(a)+1):
+    for j in range(1,len(b)+1):
+
+        if b[j-1] == a[i-1]:
+
+            dp[i][j] = dp[i-1][j-1]
+
+        else:
+            dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
+
+print(dp[len(a)][len(b)])
+
+for i in dp:
+    print(i)
