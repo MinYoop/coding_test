dc6406543ffd58917aa0e2141b847d5fe6622b39
MinYoop <myk430s@naver.com>
2021-10-22 21:24:38 +0900

    10/22 실전문제 이진탐색 떡볶이 떡 만들기, 부품찾기 2회독 풀이

--- PART 2/이진 탐색/떡볶이 떡 만들기.py
+++ PART 2/이진 탐색/떡볶이 떡 만들기.py
@@ -1,30 +1,69 @@
 
-n, m  = map(int,input().split())
+# n, m  = map(int,input().split())
 
-array = list(map(int,input().split()))
+# array = list(map(int,input().split()))
 
 
-def binary_search(array, m, start, end): 
+# def binary_search(array, m, start, end): 
 
-    result = 0
-    while start <= end:
+#     result = 0
+#     while start <= end:
             
-        mid = (start+end)//2
+#         mid = (start+end)//2
 
-        sum = 0
+#         sum = 0
         
 
-        for x in array:
-            if x > mid:
-                sum += x - mid
+#         for x in array:
+#             if x > mid:
+#                 sum += x - mid
         
-        if sum >= m:
-            result = mid
-            start = mid + 1
-        else:
-            end = mid - 1
+#         if sum >= m:
+#             result = mid
+#             start = mid + 1
+#         else:
+#             end = mid - 1
 
-    return result
+#     return result
 
 
-print(binary_search(array, m, 0, max(array)))
+# print(binary_search(array, m, 0, max(array)))
+
+
+n, m = map(int,input().split())
+
+dducks = list(map(int,input().split()))
+
+def sumDduck(x):
+
+    sum = 0
+
+    for dduck in dducks:
+        if x < dduck:
+            sum += (dduck - x)
+            
+    return sum
+
+result = 0
+
+def binary(start, end, m):
+
+    if start > end:
+        return 
+
+    global result
+    mid = (start + end) // 2    
+
+    if sumDduck(mid) < m:
+        binary(start, mid-1, m)
+    else:
+        result = mid
+        binary(mid + 1, end, m)
+
+binary(0, max(dducks), m)
+print(result)
+
+
+    
+
+
--- PART 2/이진 탐색/부품 찾기.py
+++ PART 2/이진 탐색/부품 찾기.py
@@ -1,37 +1,82 @@
 
 
-def binary_search(array, target, start, end):
+# def binary_search(array, target, start, end):
 
-    while start <= end:
+#     while start <= end:
 
-        x = (start+end)//2
+#         x = (start+end)//2
 
-        if array[x] == target:
+#         if array[x] == target:
 
-            return x
+#             return x
 
-        if target < array[x]:
+#         if target < array[x]:
 
-            end = x - 1
-        else :
-            start = x + 1
+#             end = x - 1
+#         else :
+#             start = x + 1
 
-    return None
+#     return None
+
+# n = int(input())
+
+# array = list(map(int,input().split()))
+
+# m = int(input())
+
+# targetLst = list(map(int,input().split()))
+
+# for i in targetLst:
+
+#     result = binary_search(array, i, 0, n - 1)
+
+#     if result != None:
+#         print("yes", end= " ")
+#     else:
+#         print("no", end = " ")
+
+
+
+
+
+# 두번째 풀이
 
 n = int(input())
 
-array = list(map(int,input().split()))
+lst = list(map(int,input().split()))
 
 m = int(input())
 
-targetLst = list(map(int,input().split()))
+findLst = list(map(int,input().split()))
 
-for i in targetLst:
+result = []
 
-    result = binary_search(array, i, 0, n - 1)
+lst.sort()
 
-    if result != None:
-        print("yes", end= " ")
+def binaryFind(start, end, find):
+
+    if start > end:
+        return "no"
+
+    mid = (start + end) // 2
+
+    if lst[mid] == find:
+        return "yes"
+
+    elif lst[mid] < find:         
+         return binaryFind(mid + 1, end, find)
     else:
-        print("no", end = " ")
+        return binaryFind(start, mid - 1, find)
 
+
+for i in range(m):
+    result.append(binaryFind(0, len(lst) - 1, findLst[i]))
+
+print(" ".join(result))
+
+
+
+
+
+
+    
