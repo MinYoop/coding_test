92a73ea3a5f581998554fa0c449883fb37acdcac
MinYoop <myk430s@naver.com>
2021-10-23 21:32:22 +0900

    10/23 실전문제 dp 2회독 풀이

--- PART 2/DP/개미전사.py
+++ PART 2/DP/개미전사.py
@@ -1,16 +1,36 @@
 
 
+# n = int(input())
+
+# array = list(map(int,input().split()))
+
+# dp = [0] * n
+
+# dp[0] = 1
+# dp[1] = 3
+
+# for i in range(2,n):
+
+#     dp[i] = max(array[i]+dp[i-2],dp[i-1])
+
+# print(dp[n-1])
+
+
 n = int(input())
 
-array = list(map(int,input().split()))
+lst = list(map(int,input().split()))
 
 dp = [0] * n
 
-dp[0] = 1
-dp[1] = 3
+dp[0] = lst[0]
+dp[1] = max(lst[0], lst[1])
 
 for i in range(2,n):
 
-    dp[i] = max(array[i]+dp[i-2],dp[i-1])
+    dp[i] = max(dp[i-2] + lst[i], dp[i-1])
 
 print(dp[n-1])
+
+
+
+
