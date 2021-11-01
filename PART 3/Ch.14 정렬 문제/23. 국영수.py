f19d9025b099d60e2f80ae6bf7af1513dcdb837c
MinYoop <myk430s@naver.com>
2021-11-01 21:42:02 +0900

    11/01 18,23,24 2회차 풀이

--- PART 3/Ch.14 정렬 문제/23. 국영수.py
+++ PART 3/Ch.14 정렬 문제/23. 국영수.py
@@ -1,14 +1,30 @@
-# https://www.acmicpc.net/problem/10825
+# # https://www.acmicpc.net/problem/10825
 
-n = int(input())
+# n = int(input())
 
 
-lst = []
-for i in range(n):
-    lst.append(input().split())
+# lst = []
+# for i in range(n):
+#     lst.append(input().split())
 
-lst.sort(key= lambda x: (-int(x[1]), int(x[2]), (-int(x[3])), x[0]))
+# lst.sort(key= lambda x: (-int(x[1]), int(x[2]), (-int(x[3])), x[0]))
 
-for i in lst:
-    print(i[0])
+# for i in lst:
+#     print(i[0])
 
+
+
+
+
+# 2회차 풀이
+
+# n = int(input())
+
+# lst = [list(input().split()) for _ in range(n)]
+
+# lst.sort(key = lambda x: (-int(x[1]), int(x[2]), (-int(x[3])), ord(x[0]))) 
+
+# for i in lst:
+#     print(i[0])
+
+print(chr(ord('a')))
--- PART 3/Ch.14 정렬 문제/24. 안테나.py
+++ PART 3/Ch.14 정렬 문제/24. 안테나.py
@@ -1,23 +1,31 @@
-# https://www.acmicpc.net/problem/18310
+# # https://www.acmicpc.net/problem/18310
 
-# 시간 초과 그냥 정렬 후 가운데값이 최소거리다..
+# # 시간 초과 그냥 정렬 후 가운데값이 최소거리다..
+
+# # n = int(input())
+
+# # lst = list(map(int,input().split()))
+# # distance = [0] * n
+
+# # for i in range(n):
+# #     for j in lst:
+# #         distance[i] += abs(lst[i]-j)
+
+# # resultLst = []
+# # for i in range(n):
+# #     resultLst.append([distance[i],lst[i]])
+
+# # resultLst.sort()
+
+# # print(resultLst[0][1])
+
+
 
 # n = int(input())
 
 # lst = list(map(int,input().split()))
-# distance = [0] * n
 
-# for i in range(n):
-#     for j in lst:
-#         distance[i] += abs(lst[i]-j)
-
-# resultLst = []
-# for i in range(n):
-#     resultLst.append([distance[i],lst[i]])
-
-# resultLst.sort()
-
-# print(resultLst[0][1])
+# print(sorted(lst)[(n-1)//2])
 
 
 
