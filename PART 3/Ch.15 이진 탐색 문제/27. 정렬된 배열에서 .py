222d6a29280eb98c9de3ed888904a7433801f692
MinYoop <myk430s@naver.com>
2021-11-01 21:41:45 +0900

    11/01 18, 23, 24, 27, 28 2회차 풀이

--- PART 3/Ch.15 이진 탐색 문제/27. 정렬된 배열에서 .py
+++ PART 3/Ch.15 이진 탐색 문제/27. 정렬된 배열에서 .py
@@ -1,77 +1,125 @@
 
 
-# n, x = map(int,input().split()) 
+# # n, x = map(int,input().split()) 
+# # lst = list(map(int,input().split()))
+
+# # start = 0
+# # end = n-1
+
+# # k = 0
+# # finded = False
+# # while start < end:
+
+# #     if lst[(start+end)//2] == x:
+# #         k = (start+end)//2
+# #         finded = True
+# #         break
+    
+# #     if (start+end)//2 < x:
+# #         start = (start+end)//2 + 1
+# #     else:
+# #         end = (start+end)//2 - 1
+
+# # if finded :
+
+# #     chk = True
+# #     cnt = 1
+
+# #     left = k
+
+# #     while chk:
+# #         left -= 1
+# #         if lst[left] == x:
+# #             cnt += 1
+# #         else:
+# #             chk = False
+
+# #     right = k
+
+# #     chk = True
+
+# #     while chk:
+# #         right += 1
+# #         if lst[right] == x:
+# #             cnt += 1
+# #         else:
+# #             chk = False
+
+# #     print(cnt)
+
+# # else:
+# #     print(-1)
+
+# # bisect 라이브러리 활용
+
+# from bisect import bisect_left, bisect_right
+
+# n, x = map(int,input().split())
+
 # lst = list(map(int,input().split()))
 
-# start = 0
-# end = n-1
 
-# k = 0
-# finded = False
-# while start < end:
+# def countTarget(lst, x):
 
-#     if lst[(start+end)//2] == x:
-#         k = (start+end)//2
-#         finded = True
-#         break
-    
-#     if (start+end)//2 < x:
-#         start = (start+end)//2 + 1
-#     else:
-#         end = (start+end)//2 - 1
+#     firstIndex = bisect_left(lst,x)
+#     lastIndex = bisect_right(lst,x)
 
-# if finded :
+#     return lastIndex - firstIndex
 
-#     chk = True
-#     cnt = 1
+# cnt = countTarget(lst,x)
 
-#     left = k
-
-#     while chk:
-#         left -= 1
-#         if lst[left] == x:
-#             cnt += 1
-#         else:
-#             chk = False
-
-#     right = k
-
-#     chk = True
-
-#     while chk:
-#         right += 1
-#         if lst[right] == x:
-#             cnt += 1
-#         else:
-#             chk = False
-
+# if cnt == 0:
+#     print(-1)
+# else:
 #     print(cnt)
 
-# else:
-#     print(-1)
 
-# bisect 라이브러리 활용
-
-from bisect import bisect_left, bisect_right
-
-n, x = map(int,input().split())
+n, x =  map(int,input().split())
 
 lst = list(map(int,input().split()))
 
+def left_binary(lst, start, end, x):
+    global left
 
-def countTarget(lst, x):
+    if start > end:
+        return -1
+    mid = (end + start) // 2
 
-    firstIndex = bisect_left(lst,x)
-    lastIndex = bisect_right(lst,x)
+    print("mid :",  mid)
+    
+    if lst[mid] == x and (mid == 0 or lst[mid - 1] < x):
+        
+        return mid
 
-    return lastIndex - firstIndex
+    if lst[mid] >= x:
+        return left_binary(lst, start, mid - 1, x)
+    else:
+        return left_binary(lst, mid + 1, end, x)
 
-cnt = countTarget(lst,x)
+def right_binary(lst, start, end, x):
+    global right
 
-if cnt == 0:
+    if start > end:
+        return -1
+    mid = (end + start) // 2
+    
+    if lst[mid] == x and (mid == n-1 or lst[mid + 1] > x):
+        
+        return mid
+
+    if lst[mid] <= x:
+        return right_binary(lst, mid + 1, end, x)
+    else:
+        return right_binary(lst, start, mid - 1, x)
+
+
+right = right_binary(lst, 0 ,len(lst)-1,x)
+left = left_binary(lst, 0 ,len(lst)-1,x)
+
+if left == -1:
     print(-1)
 else:
-    print(cnt)
+    print(right - left + 1)
 
 
 
--- PART 3/Ch.15 이진 탐색 문제/28. 고정점 찾기.py
+++ PART 3/Ch.15 이진 탐색 문제/28. 고정점 찾기.py
@@ -1,19 +1,26 @@
 
-n = int(input())
-lst = list(map(int,input().split()))
+# n = int(input())
+# lst = list(map(int,input().split()))
 
 
-def binary(start, end):
+# def binary(start, end):
     
-    if start > end:
-        return -1
-    mid = (start + end) // 2
+#     if start > end:
+#         return -1
+#     mid = (start + end) // 2
 
-    if mid == lst[mid]:
-        return mid
-    elif mid < lst[mid]:
-        return binary(start, mid-1)
-    else:
-        return binary(mid+1, end)
+#     if mid == lst[mid]:
+#         return mid
+#     elif mid < lst[mid]:
+#         return binary(start, mid-1)
+#     else:
+#         return binary(mid+1, end)
 
-print(binary(0,n-1))
+# print(binary(0,n-1))
+
+
+
+
+
+
+
