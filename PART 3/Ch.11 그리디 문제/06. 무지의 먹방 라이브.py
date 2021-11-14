a0d531fc374252ab83351ff2a65ff9c3d6232f66
MinYoop <myk430s@naver.com>
2021-11-14 23:38:47 +0900

    11/14 그리디6 무지의 먹방 라이브, 구현 10. 자물쇠와 열쇠 2회차 풀이

--- PART 3/Ch.11 그리디 문제/06. 무지의 먹방 라이브.py
+++ PART 3/Ch.11 그리디 문제/06. 무지의 먹방 라이브.py
@@ -1,25 +1,62 @@
-# 내가 푼 답 , 프로그래머스 실행결과는 통과, 효율성 테스트 실패...
+# # 내가 푼 답 , 프로그래머스 실행결과는 통과, 효율성 테스트 실패...
+
+# def solution(food_times, k):
+    
+#     cnt = 0
+#     p = 0
+    
+#     while True:
+        
+#         if food_times[p] != 0:
+#             if cnt == k:
+#                 break
+#             food_times[p] -= 1
+#             cnt += 1
+            
+#         p += 1
+#         if p == len(food_times):
+#             p = 0
+        
+#         if sum(food_times) == 0:
+#             return -1
+    
+#     return p+1
+
+#     ## 다시 풀어야함.
+
+# print(solution([3, 1, 2],5))
+# 2회차 
+# 
+
+
+import heapq
 
 def solution(food_times, k):
-    
-    cnt = 0
-    p = 0
-    
-    while True:
-        
-        if food_times[p] != 0:
-            if cnt == k:
-                break
-            food_times[p] -= 1
-            cnt += 1
-            
-        p += 1
-        if p == len(food_times):
-            p = 0
-        
-        if sum(food_times) == 0:
-            return -1
-    
-    return p+1
 
-    ## 다시 풀어야함.
+    # 전체 음식을 먹는 시간보다 k가 크거나 같다면 -1
+    if sum(food_times) <= k:
+        return -1
+
+    q = []
+    for i in range(len(food_times)):
+        # (음식 시간, 음식 번호) 형태로 우선순위 큐에 삽입
+        heapq.heappush(q, (food_times[i], i+1 ))
+    
+    sum_value = 0 # 먹기 위해 사용한 시간
+    previous = 0 # 직전에 다 먹은 음식 시간
+
+    length = len(food_times)
+
+    while sum_value + (q[0][0] - previous) * length <= k: 
+        now = heapq.heappop(q)[0]
+        sum_value += (now - previous) * length
+        length -= 1 # 다 먹은 음식 제외
+        previous = now # 이전 음식 시간 재설정
+
+    result = sorted(q, key = lambda x: x[1]) # 음식 번호 기준으로 정렬
+
+    # 남은 음식 중에서 몇 번째 음식인지 확인하여 출력
+
+    return result[(k - sum_value) % length][1]
+
+print(solution([3, 1, 2],5))
--- PART 3/Ch.12 구현 문제/10. 자물쇠와 열쇠.py
+++ PART 3/Ch.12 구현 문제/10. 자물쇠와 열쇠.py
@@ -1,55 +1,121 @@
-from copy import deepcopy
-def rotate(key):
-        rKey = [[0] * len(key) for _ in range(len(key))]
+# from copy import deepcopy
+# def rotate(key):
+#         rKey = [[0] * len(key) for _ in range(len(key))]
 
-        for i in range(len(key)):
-            for j in range(len(key)):
+#         for i in range(len(key)):
+#             for j in range(len(key)):
 
-                rKey[j][len(key)-1-i] = key[i][j]
+#                 rKey[j][len(key)-1-i] = key[i][j]
 
-        return rKey
+#         return rKey
 
-def check(new_lock):
+# def check(new_lock):
 
-    lock_length = len(new_lock) // 3
+#     lock_length = len(new_lock) // 3
 
-    for i in range(lock_length, lock_length * 2):
-        for j in range(lock_length, lock_length * 2):
+#     for i in range(lock_length, lock_length * 2):
+#         for j in range(lock_length, lock_length * 2):
 
-            if new_lock[i][j] != 1:
-                return False
-    return True
+#             if new_lock[i][j] != 1:
+#                 return False
+#     return True
 
-def solution(key, lock):
-    n = len(lock)
-    m = len(key)
+# def solution(key, lock):
+#     n = len(lock)
+#     m = len(key)
 
-    new_lock = [[0]* (n*3) for _ in range(n * 3)]
+#     new_lock = [[0]* (n*3) for _ in range(n * 3)]
+
+#     for i in range(n):
+#         for j in range(n):
+#             new_lock[i+n][j+n] = lock[i][j]
+
+#     for rotation in range(4):
+#         key = rotate(key)
+
+#         for x in range(n * 2):
+#             for y in range( n * 2):
+
+#                 for i in range(m):
+#                     for j in range(m):
+#                         new_lock[x+i][y+j] += key[i][j]
+
+#                 if check(new_lock) == True:
+#                     return True
+
+#                 for i in range(m):
+#                     for j in range(m):
+#                         new_lock[x+i][y+j] -= key[i][j]
+           
+#     return False
+    
+       
+
+# print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]],[[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
+
+
+#2회차 풀이 97점 뜸;
+
+def rotate(lst, m):
+    result = [[0] * m for _ in range(m)]
+
+    for i in range(m):
+        for j in range(m):
+            result[j][m-1-i] = lst[i][j]   
+    
+    return result
+
+
+def sumCal(board, n, m):
+
+    sum = 0
 
     for i in range(n):
         for j in range(n):
-            new_lock[i+n][j+n] = lock[i][j]
 
-    for rotation in range(4):
-        key = rotate(key)
-
-        for x in range(n * 2):
-            for y in range( n * 2):
-
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x+i][y+j] += key[i][j]
-
-                if check(new_lock) == True:
-                    return True
-
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x+i][y+j] -= key[i][j]
-           
-    return False
+            if board[m-1 + i][m-1 + j] != 1:
+                return False
     
-       
+    return True
+
+
+def solution(key, lock):
+
+    n = len(lock)
+    m = len(key)
+
+    boardLen = n + m * 2 - 2
+    
+    board = [[0] * boardLen for _ in range(boardLen)]
+
+    for i in range(n):
+        for j in range(n):
+            board[m+i-1][m+j-1] = lock[i][j]
+
+    for _ in range(4):
+        key = rotate(key,m)
+
+        for i in range(boardLen-m):
+            for j in range(boardLen-m):
+
+                for x in range(m):
+                    for y in range(m):
+
+                        board[i+x][j+y] += key[x][y]
+
+
+                if sumCal(board, n, m):
+                    
+                    return True              
+                           
+
+                for x in range(m):
+                    for y in range(m):
+
+                        board[i+x][j+y] -= key[x][y]
+  
+    return False
+
 
 print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]],[[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
 
