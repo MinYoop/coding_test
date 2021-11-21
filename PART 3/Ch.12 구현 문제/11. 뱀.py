500993a3981170ae329286b6d58efb3f179d0ec9
MinYoop <myk430s@naver.com>
2021-11-21 20:38:00 +0900

    11/21 구현 문제 11. 뱀

--- PART 3/Ch.12 구현 문제/11. 뱀.py
+++ PART 3/Ch.12 구현 문제/11. 뱀.py
@@ -1,76 +1,148 @@
 
-n = int(input())
+# n = int(input())
 
+# k = int(input())
+
+# board = [[0] * n for _ in range(n)]
+
+# for i in range(k):
+#     x, y = map(int,input().split())
+#     board[x-1][y-1] = 1
+
+# l = int(input())
+
+# direction = []
+
+# for i in range(l):
+#     second, dl = input().split()
+#     direction.append((int(second),dl))
+    
+# snake = [[0,0]]
+
+# chk = True
+# sec = 0
+
+# dx = [0, 1, 0, -1]
+# dy = [1, 0, -1, 0]
+# dir = 0
+
+# def inbody():
+#     for piece in snake:
+
+#         if snake.count(piece) > 1:
+#             return True
+#     return False
+
+# while chk:
+
+#     # 연장
+#     sec += 1
+    
+#     snake.insert(0,[snake[0][0] + dx[dir],snake[0][1] + dy[dir]])
+    
+#     # 벽이나 몸에 박았는지 확인
+#     if snake[0][0] < 0 or snake[0][0] > n - 1 or snake[0][1] < 0 or snake[0][1] > n - 1 or inbody():
+#         break
+
+#     item = snake.pop()
+
+#     # 사과 체크
+#     if board[snake[0][0]][snake[0][1]] == 1:
+#         board[snake[0][0]][snake[0][1]] == 0
+#         snake.append(item)
+        
+
+#     # 방향 체크 및 바꾸기
+#     if len(direction) != 0:
+
+#         if sec == direction[0][0]:
+            
+#             #print("방향전환")
+
+#             if direction[0][1] == 'D':
+#                 dir += 1
+#                 if dir == 4:
+#                     dir = 0
+#             else:
+#                 dir -= 1
+#                 if dir == -1:
+#                     dir = 3            
+
+#             direction.pop(0)
+
+# print(sec)
+    
+from collections import deque
+
+n = int(input())
 k = int(input())
 
-board = [[0] * n for _ in range(n)]
+lst = list()
+changes = list()
+snake = deque()
 
 for i in range(k):
     x, y = map(int,input().split())
-    board[x-1][y-1] = 1
+    lst.append((x,y))
 
 l = int(input())
 
-direction = []
-
 for i in range(l):
-    second, dl = input().split()
-    direction.append((int(second),dl))
-    
-snake = [[0,0]]
-
-chk = True
-sec = 0
+    x, c = input().split()
+    changes.append((int(x), c))
 
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
-dir = 0
 
-def inbody():
-    for piece in snake:
+snake.append((1,1)) # 오른쪽이 머리
+direction = 0
+time = 0
+while True:
+    print(snake)
+    time += 1
+    cnt = None
 
-        if snake.count(piece) > 1:
-            return True
-    return False
+    x = snake[-1][0] + dx[direction]
+    y = snake[-1][1] + dy[direction]
 
-while chk:
+    # 자기랑 부딪히는지 확인
 
-    # 연장
-    sec += 1
-    
-    snake.insert(0,[snake[0][0] + dx[dir],snake[0][1] + dy[dir]])
-    
-    # 벽이나 몸에 박았는지 확인
-    if snake[0][0] < 0 or snake[0][0] > n - 1 or snake[0][1] < 0 or snake[0][1] > n - 1 or inbody():
+    if (x,y) in snake:
         break
 
-    item = snake.pop()
+    # 벽 확인
 
-    # 사과 체크
-    if board[snake[0][0]][snake[0][1]] == 1:
-        board[snake[0][0]][snake[0][1]] == 0
-        snake.append(item)
-        
+    if 1 <= x <= n and 1 <= y <= n: 
 
-    # 방향 체크 및 바꾸기
-    if len(direction) != 0:
+        snake.append((x,y))
+        cnt = snake.popleft()
 
-        if sec == direction[0][0]:
-            
-            #print("방향전환")
+    else:
+        break
 
-            if direction[0][1] == 'D':
-                dir += 1
-                if dir == 4:
-                    dir = 0
+    # 사과 확인
+
+    if (x,y) in lst:
+        snake.appendleft(cnt)
+        lst.remove((x,y))
+
+    # 변화 확인
+
+    for change in changes:
+        if change[0] == time:
+
+            if change[1] == 'L':
+                direction -= 1
+                if direction == -1:
+                    direction = 3
             else:
-                dir -= 1
-                if dir == -1:
-                    dir = 3            
+                direction += 1
+                if direction == 4:
+                    direction = 0
 
-            direction.pop(0)
+print(time)
 
-print(sec)
     
 
 
+    
