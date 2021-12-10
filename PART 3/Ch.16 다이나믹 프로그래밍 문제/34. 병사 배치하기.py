1eb2ccfa4932b5d2af9ec5d81b3c3940eee45c6a
MinYoop <myk430s@naver.com>
2021-12-10 23:51:34 +0900

    2021.12.10 dp 문제 병사 배치하기

--- PART 3/Ch.16 다이나믹 프로그래밍 문제/34. 병사 배치하기.py
+++ PART 3/Ch.16 다이나믹 프로그래밍 문제/34. 병사 배치하기.py
@@ -1,16 +1,44 @@
 # d[i] = array[i]를 마지막 원소로 가지는 부분 수열의 최대 길이 
 # 가장 긴 증가하는 부분 수열
 
+# n = int(input())
+
+# array = list(map(int, input().split()))
+# array.reverse()
+
+# dp = [1] * n
+
+# for i in range(1,n):
+#     for j in range(0, i):
+#         if array[j] < array[i]:
+#             dp[i] = max(dp[i], dp[j] + 1)
+
+# print(n - max(dp))
+
 n = int(input())
 
 array = list(map(int, input().split()))
-
+array.reverse()
 
 dp = [1] * n
 
 for i in range(1,n):
     for j in range(0, i):
-        if array[j] > array[i]:
+        if array[j] < array[i]:
             dp[i] = max(dp[i], dp[j] + 1)
 
 print(n - max(dp))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
