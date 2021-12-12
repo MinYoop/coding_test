0d18ee6f20cc64ceebe4c5c5d52e3166b92431f7
MinYoop <myk430s@naver.com>
2021-12-12 22:28:05 +0900

    2021.12.12 dp 문제 35. 못생긴 수 / 36. 편집 거리 2회차 풀이

--- PART 3/Ch.16 다이나믹 프로그래밍 문제/36. 편집 거리.py
+++ PART 3/Ch.16 다이나믹 프로그래밍 문제/36. 편집 거리.py
@@ -1,28 +1,57 @@
 
-a = input()
-b = input()
+# a = input()
+# b = input()
 
-dp = [[0] * (len(b)+1) for _ in range(len(a)+1)]
+# dp = [[0] * (len(b)+1) for _ in range(len(a)+1)]
 
-for i in range(len(b)+1):
+# for i in range(len(b)+1):
 
-    dp[0][i] = i
+#     dp[0][i] = i
 
-for i in range(len(a)+1):
+# for i in range(len(a)+1):
 
-    dp[i][0] = i
+#     dp[i][0] = i
 
-for i in range(1,len(a)+1):
-    for j in range(1,len(b)+1):
+# for i in range(1,len(a)+1):
+#     for j in range(1,len(b)+1):
 
-        if b[j-1] == a[i-1]:
+#         if b[j-1] == a[i-1]:
 
-            dp[i][j] = dp[i-1][j-1]
+#             dp[i][j] = dp[i-1][j-1]
 
-        else:
-            dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
+#         else:
+#             dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
 
-print(dp[len(a)][len(b)])
+# print(dp[len(a)][len(b)])
 
 for i in dp:
     print(i)
+
+
+def edit_dist(str1, str2):
+
+    n = len(str1)
+    m = len(str2)
+
+    dp = [[0] * (m+1 for _ in range(n+1))]
+
+    for i in range(1, n+1):
+        dp[i][0] = i
+
+    for j in range(1, m+1):
+        dp[0][j] = j
+    
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+
+            if str1[i-1] == str2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1])
+
+    return dp[n][m]
+
+str1 = input()
+str2 = input()
+
+print(edit_dist(str1, str2))
